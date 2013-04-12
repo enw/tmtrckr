73f757a4d3cade3bf7e61e3bbc1670acef02d335
Elliot Winard <elliot.winard@frogdesign.com>
2013-04-12 15:21:12 -0400

    Add creating new ones

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,16 +9,21 @@ angular.module('tmtrkrApp')
       'Karma'
     ];
 */
+
+
+      // FILTER
       $scope.isDone = function (item) {
 	  return item.done;
       };
 
+
+      // MODEL
       $scope.tasks = [
 	  {what:"Create scaffolding using yo", done:true, active:true}, 
 	  {what:"add ability to start work", done:true, active:true}, 
 	  {what:"add ability to complete task", done:true, active:true}, 
 	  {what:"add done filter", done:true, active:true}, 
-	  {what:"Add creating new ones", done:false, active:false}, 
+	  {what:"Add creating new ones", done:true, active:false}, 
 	  {what:"Add creating new ones (multiple views + routing)", done:false, active:false}, 
 	  {what:"Add datestamp", done:false, active:false}, 
 	  {what:"Add localstorage", done:false, active:false}, 
@@ -27,6 +32,7 @@ angular.module('tmtrkrApp')
 	  {what:"Add timetracking", done:false, active:false}, 
       ];
 
+      // EVENTS AND HELPERS
       $scope.start = function (item) {
 	  item.active = true;
       }
@@ -38,4 +44,11 @@ angular.module('tmtrkrApp')
       $scope.complete = function(item) {
 	  item.done = true;
       }
+
+      $scope.input = "eh?";
+      $scope.add = function (what) {
+	  console.log("ADD", what);
+	  $scope.tasks.push({what:what,done:false, active:false});
+      }
+
   });
