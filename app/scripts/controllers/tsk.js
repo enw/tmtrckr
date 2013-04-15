2900112c5f801c75b2a6c816aff19f8a744e5c32
Elliot Winard <github@caveteen.com>
2013-04-15 13:12:14 -0400

    add UI for adding on separate page

--- app/scripts/controllers/tsk.js
+++ app/scripts/controllers/tsk.js
@@ -3,5 +3,10 @@
 angular.module('tmtrkrApp')
   .controller('TskCtrl', function ($scope, $routeParams) {
       console.log ("TSK CNTRL", $routeParams);
+      $scope.task = {what:"", done:false, active:false};
+      
+      $scope.add = function () {
+	  console.log("add", $scope.task);
+      };
   });
 
